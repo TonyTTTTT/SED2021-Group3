34038adcc8bf44b08a3d73106d404f39546504cf
fv1230 <r09942085@g.ntu.edu.tw>
2021-11-15 21:50:37 +0800

    add association class Charge
    modify code to the professor's advice.

---
+++ hw6-mapping/p65/Charge.java
@@ -0,0 +1,55 @@
+package p65;
+
+import java.util.Timer;
+
+import p65.Person;
+
+public class Charge {
+    private Timer timer;
+    private int salary;
+    private Person person;
+    private Project project;
+
+    public Charge(Timer timer, int salary, Person person, Project project) {
+        this.timer = timer;
+        this.salary = salary;
+        this.person = person;
+        this.project = project;
+    }
+
+    public int chargeTime(Project project, Timer time) {
+        return salary * time.chargeTime(project, project.getDepartment().getName());
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
--- hw6-mapping/p65/Worker.java
+++ hw6-mapping/p65/Worker.java
@@ -2,7 +2,8 @@ package p65;
 
 public class Worker extends Person{
     
-    private Set<Project> projects;
+    // private Set<Project> projects;
+    private Set<Charge> charges;
     public Worker(
         String name,
         String address, 
@@ -10,11 +11,29 @@ public class Worker extends Person{
         Set<Company> companies,  
         Set<Project> projects) {
             super(name, address, socialSecurityNumber, companies);
-            this.projects = projects;
+            // this.projects = projects;
     }
 
-    // Project operations getter and setter
-    public Set<Project> getProjects() {
-        return projects;
+    // Charge operations getter and setter
+    public Set<Charge> getCharges() {
+        return charges;
     }
+
+    public void setCharges(Set<Charge> charges) {
+        this.charges = charges;
+    }
+
+    public int chargeTime(Project project, Timer time) {
+        int charge = 0;
+        for (Charge c : charges) {
+            if (c.getProject().equals(project)) {
+                charge += c.getCharge(time);
+            }
+        }
+        return charge;
+    }
+
+    // public Set<Project> getProjects() {
+    //     return projects;
+    // }
 }
