a3493c640ab8a5c7daeed728abec8028e3e0ccbd
SED2021_group3 <sed2021_group3@csie.ntu.edu.tw>
2021-11-15 22:51:57 +0800

    Upload new file

---
+++ hw6-mapping/q6/Project.java
@@ -0,0 +1,15 @@
+import q6;
+import java.util.Set;
+
+public class Project {
+    private Set<Person> programmer;
+    private Set<ProgrammingLanguage> language;
+
+    public addProgrammer(Person p) {
+        programmer.add(p);
+    }
+
+    public addLanguage(ProgrammingLanguage new_language) {
+        language.add(new_language);
+    }
+}
