85cb771c2328b436295e566b4949bda465913547
B07902109 張翔文 <b07902109@ntu.edu.tw>
2021-12-18 11:34:18 +0800

    Done RTF Document

--- RTFConverter/Team3/Main.java
+++ RTFConverter/Team3/Main.java
@@ -2,28 +2,35 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(args[0])));
 
+        ArrayList<String> inputs = new ArrayList<String>();
         while (true) {
             try {
                 String line = bufferedReader.readLine();
-                if (line.equals("TeX")) {
-                    String data = bufferedReader.readLine();
-                    // TODO: transform data into Tex
-                    System.out.println(data);
-                } else if (line.equals("TextWidget")) {
-                    String data = bufferedReader.readLine();
-                    // TODO: transform data into TextWidget
-                    System.out.println(data);
-                } else
+                if (line == null)
+                    break;
+                if (!line.equals("TeX") && !line.equals("TextWidget")) {
                     System.out.println("Error");
+                    continue;
+                }
+
+                String data = bufferedReader.readLine();
+                if (data == null)
+                    break;
+                inputs.add(line);
+                inputs.add(data);
             } catch (Exception e) {
                 break;
             }
         }
         bufferedReader.close();
+
+        RTFReader reader = new RTFReader(inputs);
+        reader.convert();
     }
 }
